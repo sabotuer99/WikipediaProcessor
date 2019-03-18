48a9114115f8140aec69e67284fd7181bc2f7c1f
troy <sabotuer99@gmail.com>
2019-03-17 17:06:46 -0700

    first pass

--- build.sbt
+++ build.sbt
@@ -11,3 +11,4 @@ libraryDependencies += "org.scalactic" %% "scalactic" % "3.0.5"
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.5" % "test"
 // libraryDependencies += "com.novocode" % "junit-interface" % "0.8" % "test->default"
 libraryDependencies += "junit" % "junit" % "4.10" % Test
+libraryDependencies += "org.scala-lang.modules" %% "scala-xml" % "1.0.6"
--- src/main/scala/wikiprocessor/Processor.scala
+++ src/main/scala/wikiprocessor/Processor.scala
@@ -1,5 +1,71 @@
 package wikiprocessor
 
+import java.util.regex.Pattern
+
+import scala.xml.pull.XMLEventReader
+import scala.io.Source
+import scala.xml.pull._
+
 class Processor {
 
+
+
+
+
+}
+
+object Processor {
+  def main(args: Array[String]) {
+    val in = Source.fromFile("/home/troy/Downloads/Wikipedia Data/enwiki-20190301-pages-articles.xml")
+    val out_file = new java.io.FileOutputStream("/home/troy/Downloads/Wikipedia Data/enwiki-20190301-pages-articles.cleaned.txt")
+    val out_stream = new java.io.PrintStream(out_file)
+
+    val eventReader = new XMLEventReader(in)
+    var line = 0
+
+    val ignoredTagRange = List("ref", "math", "sub", "sup", "wikitable", "center", "small", "div")
+    val ignoredContains = List("==External links==", "==See also==", "==References==", "==Works cited==", "{{Navboxes", "{{Infobox", "|", "text-align:", "background:#", "style=")
+    val ignoredExact = List("br", "br/", "br /", "blockquote", "/blockquote")
+    val ignoredPrefix = List("!--")
+
+    def find(p: XMLEvent => Boolean): Option[XMLEvent] = {
+      if (eventReader.hasNext) {
+        val xml:XMLEvent = eventReader.next()
+        line += 1
+        if (p(xml)) Some(xml) else find(p)
+      }
+      else None
+    }
+
+    while (eventReader.hasNext) {
+      eventReader.next() match {
+        case EvElemStart(_,"text",_,_) => {
+          // Process page element
+          var keepProcessing = true
+          while (eventReader.hasNext && keepProcessing){
+            eventReader.next() match {
+              case EvElemEnd(_,"text") => keepProcessing = false
+              case EvText(text: String) if ignoredTagRange.exists(text.startsWith) =>
+                find({case EvText(stop) if ignoredTagRange.exists(x => stop.startsWith("/" + x)) => true; case _ => false})
+              case EvText(text: String) if ignoredContains.exists(text.contains) => {}
+              case EvText(text: String) if ignoredExact.contains(text) => {}
+              case EvText(text: String) if ignoredPrefix.exists(x => text.startsWith(x)) => {}
+              case EvText(text) => {
+                line += 1
+                val cleaned = text.
+                  replaceAll("(\\[\\[)|((|.*)?\\]\\])|(\\{\\{.*?\\}\\})|(===?=?)|('''?)|(\\n)|(nbsp;)", "")//.
+                  //toLowerCase.replaceAll("[^a-z0-9'\\- ]", "")
+                out_stream.print(cleaned)
+                //println(line + ":" + cleaned)
+              }
+              case _ => {}
+            }
+          }
+        }
+        case _ => {}
+      }
+    }
+
+    out_stream.close
+  }
 }
