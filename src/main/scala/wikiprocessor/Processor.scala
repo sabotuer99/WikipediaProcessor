13fb7570fc66a39b1274958eac5008627390a8a6
troy <sabotuer99@gmail.com>
2019-03-20 00:22:24 -0700

    divide text into 100 equalish parts

--- src/main/scala/wikiprocessor/Processor.scala
+++ src/main/scala/wikiprocessor/Processor.scala
@@ -29,6 +29,8 @@ object Processor {
     var lastCheckpoint = 0
     var bigram_epoch = 0
 
+    val segmenter = new Segmenter("/home/troy/Downloads/Wikipedia Data/bigrams/enwiki-20190301-pages-articles.bigrams", 100)
+
     val ignoredTagRange = List("ref", "math", "sub", "sup", "wikitable", "center", "small", "div")
     val ignoredContains = List("==External links==", "==See also==", "==References==", "==Works cited==", "{{Navboxes", "{{Infobox", "|", "text-align:", "background:#", "style=")
     val ignoredExact = List("br", "br/", "br /", "blockquote", "/blockquote")
@@ -43,27 +45,27 @@ object Processor {
       else None
     }
 
-    def writeLineCounts = {
-      val counts_file = new java.io.FileOutputStream("/home/troy/Downloads/Wikipedia Data/enwiki-20190301-pages-articles.counts.txt")
-      val counts_stream = new java.io.PrintStream(counts_file)
-      val word_count = wordCounts.values.sum
-      wordCounts.filter(_._2 > 1).foreach(entry => counts_stream.println(entry._1 + " " + entry._2))
-      counts_stream.print(word_count)
-      counts_stream.close
-      println("Checkpoint at line " + line + ", word count " + word_count)
-      word_count
-    }
-
-    def writeBigrams = {
-      println("Dumping bigrams after line " + line)
-      val bigram_file = new java.io.FileOutputStream("/home/troy/Downloads/Wikipedia Data/enwiki-20190301-pages-articles.bigrams" + bigram_epoch + ".txt")
-      val bigram_stream = new java.io.PrintStream(bigram_file)
-      bigramCounts.filter(_._2 > 1).foreach(entry => bigram_stream.println(entry._1 + " " + entry._2))
-      bigram_stream.print(bigramCounts.values.sum)
-      bigram_stream.close
-      bigramCounts.clear()
-      bigram_epoch += 1
-    }
+//    def writeLineCounts = {
+//      val counts_file = new java.io.FileOutputStream("/home/troy/Downloads/Wikipedia Data/enwiki-20190301-pages-articles.counts.txt")
+//      val counts_stream = new java.io.PrintStream(counts_file)
+//      val word_count = wordCounts.values.sum
+//      wordCounts.filter(_._2 > 1).foreach(entry => counts_stream.println(entry._1 + " " + entry._2))
+//      counts_stream.print(word_count)
+//      counts_stream.close
+//      println("Checkpoint at line " + line + ", word count " + word_count)
+//      word_count
+//    }
+//
+//    def writeBigrams = {
+//      println("Dumping bigrams after line " + line)
+//      val bigram_file = new java.io.FileOutputStream("/home/troy/Downloads/Wikipedia Data/enwiki-20190301-pages-articles.bigrams" + bigram_epoch + ".txt")
+//      val bigram_stream = new java.io.PrintStream(bigram_file)
+//      bigramCounts.filter(_._2 > 1).foreach(entry => bigram_stream.println(entry._1 + " " + entry._2))
+//      bigram_stream.print(bigramCounts.values.sum)
+//      bigram_stream.close
+//      bigramCounts.clear()
+//      bigram_epoch += 1
+//    }
 
     while (eventReader.hasNext) {
       eventReader.next() match {
@@ -71,17 +73,17 @@ object Processor {
           // Process page element
           var keepProcessing = true
 
-          if(line - lastCheckpoint > 10000000){
-            val word_count: Int = writeLineCounts
-
-            // Every 250000000 words, dump and reset the bigram hashmap
-            // This is to avoid OutOfMemoryException
-            if(word_count > 250000000 * (bigram_epoch + 1)){
-              writeBigrams
-            }
-
-            lastCheckpoint = line
-          }
+//          if(line - lastCheckpoint > 10000000){
+//            val word_count: Int = writeLineCounts
+//
+//            // Every 250000000 words, dump and reset the bigram hashmap
+//            // This is to avoid OutOfMemoryException
+//            if(word_count > 250000000 * (bigram_epoch + 1)){
+//              writeBigrams
+//            }
+//
+//            lastCheckpoint = line
+//          }
 
           while (eventReader.hasNext && keepProcessing){
             eventReader.next() match {
@@ -107,18 +109,19 @@ object Processor {
                   replaceAll("[0-9]","").
                   split(" ", -1)
 
-                // count individual words
-                trimmed.
-                  map(word => if (word.nonEmpty && word != " ") wordCounts.put(word, wordCounts.withDefault(_ => 0)(word) + 1))
-
-
-                // count bigrams
+//                // count individual words
+//                trimmed.
+//                  map(word => if (word.nonEmpty && word != " ") wordCounts.put(word, wordCounts.withDefault(_ => 0)(word) + 1))
+//
+//
+                // segment bigrams
                 for((a, b) <- trimmed zip trimmed.drop(1) if a.nonEmpty && b.nonEmpty) {
                   val bigram = a + " " + b
-                  bigramCounts.put(bigram, bigramCounts.withDefault(_ => 0)(bigram) + 1)
+                  //bigramCounts.put(bigram, bigramCounts.withDefault(_ => 0)(bigram) + 1)
+                  segmenter.write(bigram)
                 }
 
-                out_stream.print(cleaned)
+                out_stream.println(cleaned)
                 //println(line + ":" + cleaned)
               }
               case _ => {}
@@ -128,8 +131,9 @@ object Processor {
         case _ => {}
       }
     }
-    writeLineCounts
-    writeBigrams
+//    writeLineCounts
+//    writeBigrams
     out_stream.close
+    segmenter.closeAll
   }
 }
--- src/main/scala/wikiprocessor/Segmenter.scala
+++ src/main/scala/wikiprocessor/Segmenter.scala
@@ -1,5 +1,15 @@
 package wikiprocessor
 
-class Segmenter {
+import scala.util.hashing.MurmurHash3
+
+class Segmenter(baseFile: String, segments: Int) {
+  val streamMap = (for (i <- 0 until segments) yield (i, new java.io.PrintStream(new java.io.FileOutputStream(baseFile + "_" + i + ".txt")))).toMap
+
+  def closeAll = streamMap.foreach(_._2.close())
+
+  def write(line: String): Unit ={
+    val index = Math.abs(MurmurHash3.stringHash(line)) % segments
+    streamMap(index).println(line)
+  }
 
 }
