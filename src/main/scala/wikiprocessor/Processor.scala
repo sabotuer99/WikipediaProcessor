32a5bf21b18de829898b8213552f50a6d723263d
troy <sabotuer99@gmail.com>
2019-03-20 22:36:15 -0700

    reworking my approach since it keeps hanging

--- src/main/scala/wikiprocessor/Processor.scala
+++ src/main/scala/wikiprocessor/Processor.scala
@@ -16,124 +16,68 @@ class Processor {
 }
 
 object Processor {
+  def clean(raw: String): String = {
+    val eraserRegex = List(
+      "<ref( name=)?>.*?</ref>",
+      "<ref.*?/>",
+      "</ref>",
+      "<ref name=Note.*?>",
+      "<#REDIRECT.*?>",
+      "== Sources ==.*",
+      "== References ==.*",
+      "=?==.*?===?",
+      "\\{\\{.*?\\}\\}:?",
+      "'''?",
+      "\\[\\[File:.*?\n",
+      "\\[\\[Category:.*?\\]\\]",
+      "\\[{2}[^|\\]]+[|]",
+      "\\.\\.\\.",
+      "\\[https?:.*?\\]",
+      "<!--.*?-->",
+      "<\n\\|.*?>"
+    )
+
+    raw.
+      replaceAll("(?s)(" + eraserRegex.mkString(")|(") + ")" , "").
+      replaceAll("nbsp;", " ").
+      replaceAll("[^0-9A-Za-z\\- \n.']", " ").
+      replaceAll(" +", " ").
+      replaceAll("\n+", "\n").
+      trim
+  }
+
   def main(args: Array[String]) {
     val in = Source.fromFile("/home/troy/Downloads/Wikipedia Data/enwiki-20190301-pages-articles.xml")
     val out_file = new java.io.FileOutputStream("/home/troy/Downloads/Wikipedia Data/enwiki-20190301-pages-articles.cleaned.txt")
     val out_stream = new java.io.PrintStream(out_file)
 
-    val wordCounts: mutable.HashMap[String, Int] = mutable.HashMap()
-    val bigramCounts: mutable.HashMap[String, Int] = mutable.HashMap()
-
     val eventReader = new XMLEventReader(in)
-    var line = 0
-    var lastCheckpoint = 0
-    var bigram_epoch = 0
+    var line = 0;
 
     val segmenter = new Segmenter("/home/troy/Downloads/Wikipedia Data/bigrams/enwiki-20190301-pages-articles.bigrams", 100)
 
-    val ignoredTagRange = List("ref", "math", "sub", "sup", "wikitable", "center", "small", "div")
-    val ignoredContains = List("==External links==", "==See also==", "==References==", "==Works cited==", "{{Navboxes", "{{Infobox", "|", "text-align:", "background:#", "style=")
-    val ignoredExact = List("br", "br/", "br /", "blockquote", "/blockquote")
-    val ignoredPrefix = List("!--")
 
-    def find(p: XMLEvent => Boolean): Option[XMLEvent] = {
-      if (eventReader.hasNext) {
-        val xml:XMLEvent = eventReader.next()
-        line += 1
-        if (p(xml)) Some(xml) else find(p)
-      }
-      else None
+
+    def nextLine: XMLEvent = if (eventReader.hasNext) eventReader.next() else null
+
+    def getUntilEndTag(tagName: String): String = nextLine match {
+      case null => ""
+      case EvElemEnd(_, tag) if tag == tagName => ""
+      case EvText(text: String) => "<" + text + ">" + getUntilEndTag(tagName)
+      case _ => getUntilEndTag(tagName)
     }
-
-//    def writeLineCounts = {
-//      val counts_file = new java.io.FileOutputStream("/home/troy/Downloads/Wikipedia Data/enwiki-20190301-pages-articles.counts.txt")
-//      val counts_stream = new java.io.PrintStream(counts_file)
-//      val word_count = wordCounts.values.sum
-//      wordCounts.filter(_._2 > 1).foreach(entry => counts_stream.println(entry._1 + " " + entry._2))
-//      counts_stream.print(word_count)
-//      counts_stream.close
-//      println("Checkpoint at line " + line + ", word count " + word_count)
-//      word_count
-//    }
-//
-//    def writeBigrams = {
-//      println("Dumping bigrams after line " + line)
-//      val bigram_file = new java.io.FileOutputStream("/home/troy/Downloads/Wikipedia Data/enwiki-20190301-pages-articles.bigrams" + bigram_epoch + ".txt")
-//      val bigram_stream = new java.io.PrintStream(bigram_file)
-//      bigramCounts.filter(_._2 > 1).foreach(entry => bigram_stream.println(entry._1 + " " + entry._2))
-//      bigram_stream.print(bigramCounts.values.sum)
-//      bigram_stream.close
-//      bigramCounts.clear()
-//      bigram_epoch += 1
-//    }
 
     while (eventReader.hasNext) {
       eventReader.next() match {
-        case EvElemStart(_,"text",_,_) => {
-          // Process page element
-          var keepProcessing = true
+        case EvElemStart(_, "text", _, _) => {
+          val rawText = getUntilEndTag("text")
+          val allText = clean(rawText)
 
-//          if(line - lastCheckpoint > 10000000){
-//            val word_count: Int = writeLineCounts
-//
-//            // Every 250000000 words, dump and reset the bigram hashmap
-//            // This is to avoid OutOfMemoryException
-//            if(word_count > 250000000 * (bigram_epoch + 1)){
-//              writeBigrams
-//            }
-//
-//            lastCheckpoint = line
-//          }
-
-          while (eventReader.hasNext && keepProcessing){
-            eventReader.next() match {
-              case EvElemEnd(_,"text") => {
-                out_stream.print("\n")
-                keepProcessing = false
-              }
-              case EvText(text: String) if ignoredTagRange.exists(text.startsWith) => {
-                find({ case EvText(stop) if ignoredTagRange.exists(x => stop.startsWith("/" + x)) => true; case _ => false })
-                out_stream.print(" ")
-              }
-              case EvText(text: String) if ignoredContains.exists(text.contains) => out_stream.print(" ")
-              case EvText(text: String) if ignoredExact.contains(text) => out_stream.print(" ")
-              case EvText(text: String) if ignoredPrefix.exists(x => text.startsWith(x)) => out_stream.print(" ")
-              case EvText(text) => {
-                line += 1
-                val cleaned = text.
-                  replaceAll("(\\[\\[)|((|.*)?\\]\\])|(\\{\\{.*?\\}\\})|(===?=?)|('''?)|(\\n)|(nbsp;)", " ").
-                  toLowerCase.replaceAll("[^a-z0-9'\\- .]", " ")
-
-                val trimmed = cleaned.
-                  replaceAll("[.]", " ").
-                  replaceAll("[0-9]","").
-                  split(" ", -1)
-
-//                // count individual words
-//                trimmed.
-//                  map(word => if (word.nonEmpty && word != " ") wordCounts.put(word, wordCounts.withDefault(_ => 0)(word) + 1))
-//
-//
-                // segment bigrams
-                for((a, b) <- trimmed zip trimmed.drop(1) if a.nonEmpty && b.nonEmpty) {
-                  val bigram = a + " " + b
-                  //bigramCounts.put(bigram, bigramCounts.withDefault(_ => 0)(bigram) + 1)
-                  segmenter.write(bigram)
-                }
-
-                out_stream.println(cleaned)
-                //println(line + ":" + cleaned)
-              }
-              case _ => {}
-            }
-          }
+          line += 1
         }
-        case _ => {}
+        case _ => { line += 1 }
       }
     }
-//    writeLineCounts
-//    writeBigrams
-    out_stream.close
     segmenter.closeAll
   }
 }
--- src/test/scala/wikiprocessor/ProcessorSuite.scala
+++ src/test/scala/wikiprocessor/ProcessorSuite.scala
@@ -1,6 +1,24 @@
 package wikiprocessor
 
-class ProcessorSuite {
+import scala.io.Source
 
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class ProcessorSuite extends FunSuite {
+
+  val autismPath = getClass.getResource("autism.txt").getPath
+
+  test("clean autism article"){
+
+    val in = Source.fromFile(autismPath)
+    val raw = in.mkString
+
+    val clean = Processor.clean(raw)
+
+    assert(clean.startsWith("Autism is a developmental disorder"))
+  }
 
 }
