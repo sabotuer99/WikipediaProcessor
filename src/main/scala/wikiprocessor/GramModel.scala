2715cff1b1051daaaed86312105496aef3695563
troy <sabotuer99@gmail.com>
2019-04-07 19:22:54 -0700

    tested loading uni- and bi- gram models from filesystem

--- src/main/scala/wikiprocessor/GramModel.scala
+++ src/main/scala/wikiprocessor/GramModel.scala
@@ -3,20 +3,41 @@ package wikiprocessor
 import scala.collection.mutable
 import scala.io.Source
 
-class GramModel(counts: Map[String,Int]) {
+
+abstract class GramModel {
+  def getPercentage(token: String): Double
+  def getTotal: BigInt
+}
+
+
+class SingletonGramModel(counts: Map[String,Int]) extends GramModel {
   val total: BigInt = counts.values.map(BigInt.apply).sum
 
-  def getPercentage(token: String): Double = {
-    counts(token) / total.doubleValue()
+  override def getPercentage(token: String): Double = {
+    counts.withDefault(_ => 1)(token) / total.doubleValue()
   }
 
-  def getTotal: BigInt = {
+  override def getTotal: BigInt = {
+    total
+  }
+}
+
+class ShardedGramModel(allcounts: List[Map[String,Int]]) extends GramModel {
+  val total: BigInt = allcounts.map(_.values.map(BigInt.apply).sum).sum
+
+  override def getPercentage(token: String): Double = {
+    val count = allcounts.map(_.withDefault(_ => 0)(token)).sum
+    Math.max(count, 1) / total.doubleValue()
+  }
+
+  override def getTotal: BigInt = {
     total
   }
 }
 
 object GramModel {
-  def fromFile(filename: String): GramModel ={
+  private def fileToMap(filename: String): Map[String,Int] = {
+    println("Processing " + filename)
     val in = Source.fromFile(filename)
     val counts: mutable.HashMap[String, Int] = mutable.HashMap()
     var lineNum = 0
@@ -31,7 +52,17 @@ object GramModel {
         case _ =>
       }
     }
-    new GramModel(counts.toMap)
+    println("\nProcessed total: " + lineNum + "\n")
+    counts.toMap
+  }
+
+  def fromFile(filename: String): GramModel = {
+    new SingletonGramModel(fileToMap(filename))
+  }
+
+  def fromFiles(filenames: List[String]): GramModel = {
+    val maps = filenames.map(fileToMap)
+    new ShardedGramModel(maps)
   }
 }
 
--- src/test/scala/wikiprocessor/GramModelSuite.scala
+++ src/test/scala/wikiprocessor/GramModelSuite.scala
@@ -10,13 +10,33 @@ class GramModelSuite extends FunSuite {
     val unimodel = GramModel.fromFile("/home/troy/Downloads/Wikipedia Data/unigrams/enwiki-20190301-pages-articles.unigrams.counts.txt")
 
     val line = "the quick brown fox jumped over the lazy dog"
-    line.split(" ").foreach((e: String) => println(e.padTo(10, " ").mkString + ": " + "%1.9f".format(unimodel.getPercentage(e))))
+    line.split(" ").
+      map((e: String) => (e.padTo(10, " ").mkString, unimodel.getPercentage(e))).
+      distinct.
+      sortBy(_._2).
+      reverse.
+      foreach(entry => println(entry._1 + ": " + "%1.9f".format(entry._2)))
 
     println(unimodel.getTotal)
   }
 
-  test("load bigram model"){
-    //GramModel.fromFile("/home/troy/Downloads/Wikipedia Data/bigram_counts-second_attempt/all_bigram_counts.txt"
+  test("load sharded bigram model"){
+    val shards = (0 to 99).toList.map(
+      "/home/troy/Downloads/Wikipedia Data/bigram_counts-second_attempt/" +
+        "enwiki-20190301-pages-articles.bigrams.counts" + _ + ".txt"
+      )
+    val bimodel = GramModel.fromFiles(shards)
+
+    val line = "the quick brown fox jumped over the lazy dog"
+    val bigrams = line.split(" ").zip(line.split(" ").drop(1)).map(entry => entry._1 + " " + entry._2)
+    bigrams.
+      map((e: String) => (e.padTo(16, " ").mkString, bimodel.getPercentage(e))).
+      distinct.
+      sortBy(_._2).
+      reverse.
+      foreach(entry => println(entry._1 + ": " + "%1.9f".format(entry._2)))
+
+    println(bimodel.getTotal)
   }
 
 
